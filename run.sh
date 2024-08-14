d723455e93cd3d24d3af7f291d0aa6807ccd6a29
mhdmunandar <62527457+mhdmunandar@users.noreply.github.com>
2024-08-14 10:57:48 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Backup the original config.php file
+cp /DATA/AppData/big-bear-nextcloud/html/config/config.php /DATA/AppData/big-bear-nextcloud/html/config/config.php.bak
+
+# Add the LAN IP to the config.php file
+awk -v ip="$lan_ip" '/0 => '\''localhost'\''/{print; print "    1 => '\''"nextcloud.mm62.online"'\'',"; next}1' /DATA/AppData/big-bear-nextcloud/html/config/config.php.bak > /DATA/AppData/big-bear-nextcloud/html/config/config.php
+
+# Get the path to the docker-compose.yml file
+COMPOSE_FILE="/var/lib/casaos/apps/big-bear-nextcloud/docker-compose.yml"
+
+# Apply changes using casaos-cli
+casaos-cli app-management apply "big-bear-nextcloud" --file="$COMPOSE_FILE"
